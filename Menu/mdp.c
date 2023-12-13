de61917c48cac06803d0f5564c055ebb32d3c02f
theo <theo.odievre@edu.ece.fr>
2023-12-13 11:53:50 +0100

    creation menu mot de passe

---
+++ Menu/mdp.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <string.h>
+#include "mdp.h"
+void motDePasse() {
+    char chien, chat, cheval, cochon;
+    char mdp[20];
+    printf("Saisissez un mot de passe valide : ");
+    scanf("%s", &mdp);
+    if(strcmp("chien", mdp)) {
+
+    }
+    if(strcmp("chat", mdp)) {
+
+    }
+    if(strcmp("cheval", mdp)) {
+
+    }
+    if(strcmp("cochon", mdp)) {
+
+    }
+
+}
---
+++ Menu/mdp.h
@@ -0,0 +1,8 @@
+//
+// Created by User on 13/12/2023.
+//
+
+#ifndef PROJET_MDP_H
+#define PROJET_MDP_H
+void motDePasse();
+#endif //PROJET_MDP_H
