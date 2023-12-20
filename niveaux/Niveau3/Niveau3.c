6749d86946781b89d335059268f68a4b3cc7ee91
theo <theo.odievre@edu.ece.fr>
2023-12-20 12:31:02 +0100

    niveau suivant avec temps de pause affichage mdp

--- niveaux/Niveau3/Niveau3.c
+++ niveaux/Niveau3/Niveau3.c
@@ -4,5 +4,39 @@ PARTIE niveau3(PARTIE partie) {
     partie.niveau = initNiveau(3);
     partie.niveauActuel = 3;
     jouerNiveau(&partie);
+    clearConsole();
+    printf("%s%s", ANSI_YELLOW
+                   "                       #                       \n"
+                   "                      ###                      \n"
+                   "                      # #                      \n"
+                   "                     ## ##                     \n"
+                   "                    ##   ##                    \n"
+                   "                    ##    #                    \n"
+                   "                   ##     ##                   \n"
+                   "                   #       ##                  \n"
+                   "                  #         #                  \n"
+                   "  #################         ################# \n"
+                   "    ##                                   ##   \n"
+                   "      ###            BRAVO            ###      \n"
+                   "        ####                        ###        \n"
+                   "           ###                   ###           \n"
+                   "              ##               ###             \n"
+                   "              ##                ##             \n"
+                   "             ##                  #             \n"
+                   "             #         ##        ##            \n"
+                   "            ##      ### ###       ##           \n"
+                   "            #     ###      ###    ##           \n"
+                   "           ##  ###           ###   ##          \n"
+                   "           # ###                ### #          \n"
+                   "          ###                      ###         \n"
+                   "          #                          #         ");
+    gotoligcol(15,50);
+    printf("Le mot de passe est : cheval");
+    clock_t start,end;
+    start = clock();
+    end = clock();
+    while((end-start) <= 3*CLOCKS_PER_SEC) {
+        end = clock();
+    }
     return partie;
 }
